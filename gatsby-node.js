74f3b119041498abf637cfc3dc36d28da5c78c82
Marcus <macke.gw@hotmail.com>
2020-09-18 19:26:09 +0200

    Added single post functionality

--- gatsby-node.js
+++ gatsby-node.js
@@ -33,15 +33,15 @@ exports.createPages = async function({ actions, graphql }) {
         })
     })
 
-    // // Create single blog posts
+    // Create single blog posts
 
-    // data.allMdx.edges.forEach(edge => {
-    //     const slug = edge.node.frontmatter.slug
-    //     const id = edge.node.id
-    //     actions.createPages({
-    //         path: slug,
-    //         component: require.resolve("./src/templates.singlePost.js"),
-    //         context: {id},
-    //     })
-    // })
+    data.allMdx.edges.forEach(edge => {
+        const slug = edge.node.frontmatter.slug
+        const id = edge.node.id
+        actions.createPage({
+            path: slug,
+            component: require.resolve("./src/templates/singlePost.js"),
+            context: {id},
+        })
+    })
 }
